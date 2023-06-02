e9667a1085fbfc618830c30cbbc3078f4f9b1fef
NguyenNgocDuong <78304672+NguyenNgocDuongHumg@users.noreply.github.com>
2023-06-02 11:41:44 +0700

    Design App layout
    thêm home js
    tích hợp react Router-dom

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.scss";
 import Header from "./components/Header";
 import TableUsers from "./components/TableUsers";
 import { ToastContainer } from "react-toastify";
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -10,7 +12,10 @@ function App() {
       <div className="app-container">
         <Header />
         <Container>
-          <TableUsers />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/users" element={<TableUsers />} />
+          </Routes>
         </Container>
       </div>
       <ToastContainer
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <>
+      <div>
+        <h2>Chao Mung ban den voi AppTodo cua Duong</h2>
+        {/* <p>Vui long dang nhap</p> */}
+      </div>
+    </>
+  );
+}
+
+export default Home;
